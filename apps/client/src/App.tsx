0c3ebdc689fb194f0d3f953f2a40e66d1ffa73e5
alexander <alexanderglover478@gmail.com>
2025-09-10 18:22:56 +0000

    #33 E0_US4-configure CORS for communication between client and server.

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {healthCheck} from "./api/healthCheck"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+  const alertHealthCheck = async () => {
+    try{
+        const response = await healthCheck()
+        console.log(response)
+        alert(`Health check response: ${response.status} ${response.data?.message}`)
+    }catch(e){
+        console.error("health check failed", e)
+    }
+  }
   return (
     <>
       <div>
@@ -18,8 +27,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={alertHealthCheck}>
+          health check
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
